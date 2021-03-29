21af03f436d45e972a913ce6016e5738f4e72c27
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2021-03-29 16:10:30 -0700

    begining structure of zipcode functionality

--- project-folder/client/src/pages/Questionnaire.js
+++ project-folder/client/src/pages/Questionnaire.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Questionnaire () {
+    const [area, setArea] = useState('');
+    const available_areas = [{'zip': ['90210', '90038'], 'area': 'Bev Hills'}, {'zip': ['91406', '90029'], 'area': 'The Valley'}, {'zip': ['90401', '90265'], 'area': 'Beach Cities'}]
+
+
+    function addressEntered (zip) {
+        
+        let found_area = ""
+
+        for(var i = 0; i < available_areas.length; i++) {
+            let currentAreaObj = available_areas[i]
+            let currentAreaObjectZips = currentAreaObj.zip
+            for(var j = 0; j < currentAreaObjectZips.length; j++) {
+                let currentZip = currentAreaObjectZips[j]
+                console.log('current zip', currentZip)
+                console.log('zip', zip)
+                if (currentZip === zip ) {
+                    console.log('area matched', currentAreaObj.area)
+                    found_area = currentAreaObj.area
+                    break;
+                }
+                
+            }   
+        }   
+        setArea(found_area)
+        console.log('area found is ',found_area)
+      
+
+    }
+    
     return (
         <div>
             <h1>
-               Questionnaire
+                {area}
+               <input type="text" onChange={event => { addressEntered(event.target.value)}} />
             </h1>
         </div>
     )
