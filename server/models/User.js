e6d26e46a5aabe7d1f595fd42ebc66a348a8558d
eramos3 <cheddarshreddar3@gmail.com>
2021-03-29 17:46:22 -0700

    organized files

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const userPhotoSchema = require('./Photo');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: Number,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  // location: {
+  //   type: String,
+  //   required: true
+  // },
+  // gender: {
+  //   type: String,
+  //   required: true
+  // },
+  photos: [userPhotoSchema]
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/seeders/seeds.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { User } = require('../models');
+
+db.once('open', async () => {
+    await User.deleteMany({});
+
+    // create user data
+    const userData = [];
+
+    for (let i = 0; i < 25; i += 1) {
+        const name = faker.name.firstName()
+        const email = faker.internet.email(name);
+        const password = faker.internet.password();
+        const age = faker.random.number({ min: 18, max: 90 });
+        const photos = faker.internet.avatar();
+        userData.push({ name, email, password, age, photos });
+    }
+
+    const createdUsers = await User.collection.insertMany(userData);
+
+    console.log('all done!');
+    process.exit(0);
+});
