52ae457a62dcfcb1eb2e5bfb489610917cf2ef3d
ArlonTuazon <nikka1378@yahoo.com>
2021-03-29 20:44:35 -0700

    added questions.js

--- project-folder/client/src/App.js
+++ project-folder/client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from '../src/Header';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from "apollo-boost";
+
+import Questions from './components/questions';
 import  TinderCards  from '../src/TinderCards';
 import SwipeButtons from "../src/SwipeButtons";
 
+const client = new ApolloClient({
+  uri:  '/graphql'
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="app">
       <Header />
       <TinderCards />
       <SwipeButtons />
     </div>
+    </ApolloProvider>
   );
 }
 
---
+++ project-folder/server/models/Questions.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const QuestionsSchema = newSchema ({
+
+    Question1: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = Questions;
